fab522022ad0f9e1bde3cc7170d9b47e2604e3b0
then <lequanhlol@gmail.com>
2024-05-26 23:07:28 +0700

    update

--- Client/DurifyAndroid/app/src/main/java/huce/duriu/durifyandroid/MainActivity.java
+++ Client/DurifyAndroid/app/src/main/java/huce/duriu/durifyandroid/MainActivity.java
@@ -1,26 +1,58 @@
 package huce.duriu.durifyandroid;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private RecyclerView recyclerView;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
+    }
+
+    //FirstFragment firstFragment = new FirstFragment();
+
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        if (item.getItemId() == R.id.home) {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.flFragment, firstFragment)
+//                    .commit();
+            return true;
+        }
+        else if (item.getItemId() == R.id.playing) {
+            //
+            return true;
+        }
+        else if (item.getItemId() == R.id.downloaded) {
+            //
+            return true;
+        }
+        return false;
     }
 }
